616cb4c1263a16f4c951925c6891d8c423ef14d0
Vyacheslav <my1304@t-online.de>
2025-02-19 22:54:34 +0100

    Save

--- src/main/java/luxmart_backend/security/CORS/WebConfig.java
+++ src/main/java/luxmart_backend/security/CORS/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
                         .allowedOrigins("https://luxmart1.netlify.app", "http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
